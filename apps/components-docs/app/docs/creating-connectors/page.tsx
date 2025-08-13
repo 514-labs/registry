f986e3ad8337a6e6024846673c431660fa83bee9
Johanan Ottensooser <j@ottensooser.com>
2025-08-13 14:40:13 -0600

    restructure connector creation docs

--- apps/components-docs/app/docs/creating-connectors/page.tsx
+++
@@ -1,9 +0,0 @@
-import CreatingConnectors from "@/content/docs/creating-connectors.mdx";
-
-export default function CreatingConnectorsPage() {
-  return (
-    <div className="prose dark:prose-invert prose-neutral max-w-none">
-      <CreatingConnectors />
-    </div>
-  );
-}
