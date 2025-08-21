2228846d0b7401329ee696a1561bbab9dad80fee
Carlos Justiniano <carlos.justiniano@gmail.com>
2025-08-20 22:01:01 -0600

    Refactor initial-setup.sh script for improved directory navigation (#46)
    - Updated directory change commands for consistency and clarity.
    - Ensured the script navigates correctly to the app/hubspot directory before installing dependencies and building the application.

--- pipeline-registry/hubspot-to-clickhouse/v3/514-labs/typescript/default/scripts/initial-setup.sh
+++ pipeline-registry/hubspot-to-clickhouse/v3/514-labs/typescript/default/scripts/initial-setup.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-cd ..
 pnpm i
 
-cd app/hubspot
+cd ./app/hubspot
 pnpm i
 pnpm run build
-cd ../..
+cd ..
 
 echo "Review the README.md file for instructions on how to configure and run this pipeline"
