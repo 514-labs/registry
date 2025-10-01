d9f0b4a42fedb6530ad446bff864b9df081c7ed7
Alex <alex.lakas@gmail.com>
2025-10-01 09:39:49 -0700

    Remove legacy connectors-docs and pipelines-docs folders

--- apps/connectors-docs/.next/static/chunks/apps_connectors-docs_app_error_tsx_c97ddadb._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/apps_connectors-docs_app_error_tsx_80a098dc._.js"
-],
-    source: "dynamic"
-});
--- apps/connectors-docs/.next/static/chunks/apps_connectors-docs_app_favicon_ico_mjs_84bf6a21._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/c01fa_next_dist_cc79ff17._.js"
-],
-    source: "dynamic"
-});
