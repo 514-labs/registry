f2e281b3c424d3e5eb800243eea91808881d81a5
Benoit Aubuchon <ben@fiveonefour.com>
2026-01-28 12:29:14 -0500

    Export ClientTableStatus from package __init__
    The conftest.py imports ClientTableStatus but it wasn't exported from
    the package __init__.py, causing import errors in CI tests.
    
    Added ClientTableStatus to:
    - Import statement from .models
    - __all__ export list
    
    This fixes the CI test failure:
    ImportError: cannot import name 'ClientTableStatus' from 'sap_hana_cdc'

--- connector-registry/sap-hana-cdc/v1/514-labs/python/default/src/sap_hana_cdc/__init__.py
+++ connector-registry/sap-hana-cdc/v1/514-labs/python/default/src/sap_hana_cdc/__init__.py
@@ -9,7 +9,7 @@ from .connector import SAPHanaCDCConnector
 from .infrastructure import SAPHanaCDCInfrastructure
 from .reader import SAPHanaCDCReader
 from .config import SAPHanaCDCConfig
-from .models import ChangeEvent, BatchChange, PruneResult, TableStatus, TriggerType
+from .models import ChangeEvent, BatchChange, PruneResult, TableStatus, TriggerType, ClientTableStatus
 
 __all__ = [
     "SAPHanaCDCConnector",
@@ -21,4 +21,5 @@ __all__ = [
     "PruneResult",
     "TableStatus",
     "TriggerType",
+    "ClientTableStatus",
 ]
